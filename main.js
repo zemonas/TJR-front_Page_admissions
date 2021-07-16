1c25632564966075dc312f45ad62f5d569ca3b49
Abdullah Ajaz Siddiqui <abdulajaz20@gmail.com>
2021-07-16 21:33:18 +0530

    font change

--- main.js
+++ main.js
@@ -17,6 +17,17 @@ const s_one_ob = new IntersectionObserver(function(entries,s_one_ob){
 s_one_opt);
 s_one_ob.observe(s_one);
 
+//stop scrolling when full screen or half screen menue is on
+const chk_btn = document.getElementById("ch-1");
+const body = document.body;
+chk_btn.addEventListener( 'change', function() {
+    if(this.checked) {
+        body.style.overflow='hidden';
+    } else {
+        body.style.overflow='auto';
+    }
+});
+
 
 // Footer stuff
 const p = document.getElementById("copyright")
